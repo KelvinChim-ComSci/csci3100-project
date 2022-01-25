1b0b49366bcbbc034295b7255ea9dfbd733fc0d4
Kelvin Chim <Kelvin2000cc@gmail.com>
2022-01-25 23:51:25 +0800

    changed to Express

--- Test.js
+++ Test.js
@@ -1,9 +1,11 @@
-var http = require("http");
+var express = require("express");
+var app = express();
 
-http.createServer(function (request, response) {
+
+app.get('/', function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
     response.end('Hello World\n');
-}).listen(8000);
+});
 
-console.log('Server running at http://127.0.0.1:8000/')
+app.listen(8000, console.log('Server running at http://127.0.0.1:8000/'));
