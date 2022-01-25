a8846269ca7af135f494b01c175480055d54c2d1
Kelvin Chim <Kelvin2000cc@gmail.com>
2022-01-26 00:02:49 +0800

    hotfix 3rd time

--- Test.js
+++ Test.js
@@ -7,4 +7,5 @@ app.get('/', function (req, res) {
     res.send('Hello World!\n');
 });
 
-app.listen(8000, console.log('Server running at http://127.0.0.1:8000/'));
+var port = process.env.PORT||3000;
+app.listen(port, console.log('Server listening at ' + port));
