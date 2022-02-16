5a4e8e0707cd0ec1db66d8ce4efed1d618d5f263
hea0516 <hea4152@gmail.com>
2022-02-17 01:04:16 +0800

    add component Login.js for testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from "./frontend/Login.js";
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+
+        <p>below is the login component</p>
+        <Login />
       </header>
     </div>
   );
---
+++ src/frontend/Login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    async send_request() {
+        console.log('you clicked the button OoO!')
+
+    }
+
+    render() {
+        return (
+            <div>
+                -------------------------------<br></br>
+                Login Component
+                <h5>OuO Login</h5>
+                <button onClick={this.send_request}>send request to database</button>
+            </div>
+        )
+    }
+}
+
+export default Login;
