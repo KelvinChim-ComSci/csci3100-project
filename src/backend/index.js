fb792ca283cce032dda91eea7a271d911ece697b
hea0516 <hea4152@gmail.com>
2022-02-18 00:07:22 +0800

    update the link connect to mongoDB in index.js

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,18 +1,22 @@
 // use node index.js to run
+
 //const bcrypt = require("bcryptjs");
-const express = require('express');
-const app = express();
 //const cors = require('cors');
 //app.use(cors());
 //app.use(express.json());
+const dotenv = require('dotenv');
+const express = require('express');
+const app = express();
+
 app.use(express.urlencoded({ extended: true }));
+dotenv.config();
 const { MongoClient } = require('mongodb');
 
-const uri = "mongodb+srv://aua:3100ouo@cluster0.x9iai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-//const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const collection = client.db("test").collection("devices");
+    //const collection = client.db("test").collection("devices");
     console.log("Connected database!")
     // perform actions on the collection object
     //client.close();
