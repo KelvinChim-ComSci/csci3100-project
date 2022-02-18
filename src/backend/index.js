a6090e60dcb1cc54e3dbcf9a2970b93c75487654
Kelvin Chim <Kelvin2000cc@gmail.com>
2022-02-18 17:54:41 +0800

    can now access mongoDB data in backend

--- src/backend/index.js
+++ src/backend/index.js
@@ -7,24 +7,34 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
 dotenv.config();
+
 const { MongoClient } = require('mongodb');
-
 const uri = process.env.MONGODB_URI;
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    //const collection = client.db("test").collection("devices");
-    console.log("Connected database!")
-    // perform actions on the collection object
-    //client.close();
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
+
+
+
+async function main() {
+    await client.connect();
+        app.listen(2096);
+        console.log("Connected successfully to server");
+        const db = client.db("myFirstDatabase");
+        const collection = db.collection('users');
+
+        const findResult = await collection.find({}).toArray();
+        console.log('Found documents =>', findResult);
+}
 
 app.all('/*', function (req, res) {
     // When this callback function is called, send this to client
     res.send('Hello World :3');
 });
 
-const server = app.listen(2096);
+main()
+    .then(console.log('test'))
+    .catch((error) => console.log(error));
