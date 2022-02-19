4a99942f1f4d861feef3f0f422a2a6b20cc89583
gutgut2121 <2121haisakisami@gmail.com>
2022-02-20 01:30:23 +0800

    Add schema and stat bar

--- src/backend/index.js
+++ src/backend/index.js
@@ -8,6 +8,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+var mongoose = require('mongoose')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,7 +22,30 @@ const client = new MongoClient(uri, {
     useUnifiedTopology: true
 });
 
+MONGODB_URI='mongodb+srv://chim:kelvin1155126571@cluster0.x9iai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
+mongoose.connect(MONGODB_URI);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+  console.log("Mongoose is connected!");
+});
+
+// Schema
+
+var StatSchema = mongoose.Schema({ 
+    //user: { type: mongoose.Schema.Types.ObjectId, ref:'User'},
+    gpa: { type: Number, required: true },
+    sports: { type: Number, required: true },
+    happiness: { type: Number, required: true },
+    money: { type: Number, required: true },
+    });
+    
+//var User = mongoose.model('User', UserSchema);    
+var Statistic = mongoose.model('Statistic', StatSchema);
 
 
 
@@ -46,6 +70,19 @@ app.get('/login', async function (req,res) {
 
 });
 
+ //(req,res) =>{
+app.get('/stat', async function (req,res) {
+
+    Statistic.findOne({})
+        .then((data)=>{
+            console.log('Data: ', data);
+            res.json(data);
+        })
+        .catch((error) =>{
+            console.log('error: ', error);
+        });
+
+});
 
 //app.all('/*', async function (req, res) { // When this callback function is called, send this to client
 //    res.send('Hello World :3');
--- src/frontend/Main.js
+++ src/frontend/Main.js
@@ -27,11 +27,55 @@ class Main extends React.Component {
         console.log("pop message box");
     }
 
+    componentDidMount() {
+         fetch(process.env.REACT_APP_BASE_URL + "/stat", {
+            method: "GET",
+            headers: new Headers({
+                "Content-Type": 'application/json',
+                "Accept": 'application/json',
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+                "Access-Control-Allow-Credentials": true,
+            }),
+        }
+        )
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                document.getElementById("gpa").innerText = res.gpa;
+                document.getElementById("sports").innerText = res.sports;
+                document.getElementById("happiness").innerText = res.happiness;
+                document.getElementById("money").innerText = res.money;
+            })
+    }
+ 
     render() {
         return (
             <div id="main">
                 <p> Welcome to CU Simulator! </p>
-                <div style={{ top: 10, right: 10 }}> Health point: 100 </div>
+                <div style={{ top: 10, right: 10 }}> </div>
+                <table className="statchild" >
+                <thead><tr>
+                        <th scope="col">Statistics</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td>GPA :</td>
+                        <td id="gpa">?</td>
+                    </tr>
+                    <tr><td>Sports :</td>
+                        <td id="sports">?</td>
+                    </tr>
+                    <tr><td>Happiness :</td>
+                        <td id="happiness">?</td>
+                    </tr>
+                    <tr><td>Money :</td>
+                        <td id="money">?</td>
+                    </tr>
+                </tbody>
+                </table>
+
                 <button className="mainButton" onClick={this.popFriendLlist}>Friend List</button>
                 <button className="mainButton" onClick={this.popCheckStatus}>Check status</button>
                 <button className="mainButton" onClick={this.popSchdule}>Open schdule</button>
