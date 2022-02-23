49d4cec0c5e8e408695a70434d8bdace1aaa034a
Kelvin Chim <Kelvin2000cc@gmail.com>
2022-02-24 02:28:18 +0800

    added verification page for email code

---
+++ src/frontend/Verification.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Verification extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div>
+                Please check your email for Verification:
+            </div>
+        )
+    }
+}
+
+export default Verification;
