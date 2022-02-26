74869197fb50f8b0f87a79c4f15153dff4c47add
gutgut2121 <2121haisakisami@gmail.com>
2022-02-26 14:50:41 +0800

    add event text box

--- src/frontend/Event.js
+++ src/frontend/Event.js
@@ -2,23 +2,32 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './Event.css';
 
+    
 class Event extends React.Component {
+    
+
     constructor(props) {
         super(props);
         this.state = {
         }
+        
     }
 
     render() {
-        return (<div id = "event"><div className = "container topLeft"><h1  id='Location'>LOCATION</h1></div>
+      
+        return (<div id = "event">
+                <div className = "container topLeft"><h1  id='Location'>LOCATION</h1></div>
                 <div><a href="./Main" className="container topRight" >Back to main page</a></div>
+                <div className="text">
+                <svg className="corner" viewBox="0 0 88 85" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M35 3.5L65 6.5V62L0 0L35 3.5Z" fill="white"/>
+                </svg>
+                </div>
         
         
         
         
-        
-        
-        </div>
+                </div>
         )
     }
 }
