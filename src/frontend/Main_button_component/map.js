f081ed998fd34ebc50e7850988a8004c2b856909
gutgut2121 <2121haisakisami@gmail.com>
2022-02-28 15:53:51 +0800

    display map photo

--- src/frontend/Main_button_component/map.js
+++ src/frontend/Main_button_component/map.js
@@ -3,10 +3,14 @@ import "./map.css";
 
 function displayMap() {
     return (
+     
         <div id="map">
             <h2>Map</h2>
+            <div class="map-img"></div>
             <a href="./Event"> Go to NA! </a>
         </div>
+        
+       
     )
 }
 
