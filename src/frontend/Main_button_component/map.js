5ea9b1addcf6ecfe8cea93f0f8230c72b17598c9
Kelvin Chim <Kelvin2000cc@gmail.com>
2022-03-02 01:10:19 +0800

    navigator added on map.js

--- src/frontend/Main_button_component/map.js
+++ src/frontend/Main_button_component/map.js
@@ -3,12 +3,21 @@ import "./map.css";
 import { withRouter } from '../withRouter.js';
 
 class Map extends React.Component {
+    constructor(props) {
+        super(props);
+        this.navigator = this.navigator.bind(this);
+    }
+
+    navigator() {
+        this.props.navigate("../event");
+    }
+
     render(){
         return (
             <div id="map">
                 <h2>Map</h2>
                 <div className="map-img"></div>
-                <a href="./Event"> Go to NA! </a>
+                <a onClick={this.navigator}> Go to NA! </a>
             </div>
             
            
@@ -17,4 +26,4 @@ class Map extends React.Component {
 
 }
 
-export default Map;
+export default withRouter(Map);
