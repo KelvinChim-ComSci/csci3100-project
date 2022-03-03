54385dddfbde512f09e24e5bfc88856b94c86d56
lovelycutepanda <kudicky@yahoo.com.hk>
2022-03-04 00:25:22 +0800

    recover an accidental change in event compo

--- src/frontend/choiceWindow.js
+++ src/frontend/choiceWindow.js
@@ -3,18 +3,17 @@ import "./choiceWindow.css";
 
 class Choice extends React.Component {
 
-    render(){        
+    render(){
+        const choiceItems = this.props.script_answer.map((item, index) =>
+            <button key={item} onClick={()=>this.props.handleChoice(index + 1)}>
+                {item}
+            </button>
+        );
         return (
             <div>
-                <h4>Are you sure to log out?</h4>
-                <br></br>
-                <div className="d-flex justify-content-around">
-                    <button className="btn btn-success" onClick={this.userLogout}>Yes</button>
-                    <button className="btn btn-success" onClick={() => {this.setState({popUpBar : ""})}}>No</button>
-                </div>
+                {choiceItems}
             </div>
-    
-        )
+        );
     }
 }
 
