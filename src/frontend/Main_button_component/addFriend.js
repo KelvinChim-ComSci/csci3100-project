94425d7a864e62967528cf352235f2e8133831b9
Kelvin Chim <Kelvin2000cc@gmail.com>
2022-03-17 21:50:42 +0800

    initiated addFriend for friend list

---
+++ src/frontend/Main_button_component/addFriend.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class AddFriend extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>YOU HAVE NO FRIENDS HAHAHA</p>
+                <input type="text" placeholder="Player Name"></input>
+            </div>
+
+        )
+    }
+}
+
+export default AddFriend;
--- src/frontend/friendList.js
+++ src/frontend/friendList.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import AddFriend from './Main_button_component/addFriend';
 
 // fetch friends from backend, if no friends put a message to tell player to add friends by pressing the + button.
+// There will be a list of friends with no lights on and green lights depending on the status.
 
 
 class FriendList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            addFriendPopUp: "hide"
+        }
+    }
+
+
+    addFriend() {
+        if (this.state.addFriendPopUp === "hide") return;
+        return (
+            <div>
+                <div id="shadowLayer"></div>
+                <button className="closeButton" onClick={() => {this.setState({addFriendPopUp : "hide"})}}>x</button>
+                <div className="popUp">
+                    <AddFriend />
+                </div>
+            </div>
+
+        );
+    }
     render() {
         return (
             <div class="FriendList">
-                No friends. F
+                <div>
+                    <button onClick={()=>this.setState({addFriendPopUp: "show"})}> + </button>
+                </div>
+                <div class="onlineFriends">
+                </div>
+                {this.addFriend()}
             </div>
         )
     }
