c7eae31c80f22bd8fa238f02ab4e76d9051c7f9a
hea0516 <hea4152@gmail.com>
2022-04-05 18:16:06 +0800

    admin reset password part completed

--- src/frontend/AdminFunction/ShowUsers.js
+++ src/frontend/AdminFunction/ShowUsers.js
@@ -2,6 +2,7 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import Profile from "../Main_button_component/profile";
 import { statRetrievebyId } from "../statUpdater/statRetrievebyId";
+import ChangePassword from "../SignInPage/ChangePassword";
 
 class ShowUsers extends React.Component {
 
@@ -11,11 +12,14 @@ class ShowUsers extends React.Component {
             userList: [],
             popUpBar: "",
             targetStatistic: "",
-            target: null
+            target: null,
+            overflow: 1,
         }
 
         this.getUserList = this.getUserList.bind(this);
         this.displayUserList = this.displayUserList.bind(this);
+        this.resetUserPassword = this.resetUserPassword.bind(this);
+        this.handleInnerPopClose = this.handleInnerPopClose.bind(this);
         this.popUp = this.popUp.bind(this);
     }
 
@@ -46,34 +50,31 @@ class ShowUsers extends React.Component {
         //const userList = this.state.userList;
         if (this.state.userList.length !== 0) {
             return (
-                <div className="container">
-                    <table className="table table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">User ID</th>
-                                <th scope="col">Username</th>
-                                <th scope="col">Display Name</th>
-                                <th scope="col">Email</th>
-                            </tr>
-                        </thead>
-                        <tbody>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">User ID</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Display Name</th>
+                            <th scope="col">Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-                            {this.state.userList.map((data) => {
-                                return (
-                                    <tr key={data._id} onClick={async () => { await this.showUserProfile(data) }}>
-                                        <th scope="row">{data.userId}</th>
-                                        <td>{data.username}</td>
-                                        <td>{data.displayName}</td>
-                                        <td>{data.email}</td>
-                                        <td><button>Reset Password</button></td>
-                                    </tr>
+                        {this.state.userList.map((data) => {
+                            return (
+                                <tr key={data._id} onClick={async () => { await this.showUserProfile(data) }}>
+                                    <th scope="row">{data.userId}</th>
+                                    <td>{data.username}</td>
+                                    <td>{data.displayName}</td>
+                                    <td>{data.email}</td>
+                                    <td><button onClick={(e) => this.resetUserPassword(e, data)}>Reset Password</button></td>
+                                </tr>
 
-                                );
-                            })}
-                        </tbody>
-                    </table>
-
-                </div >
+                            );
+                        })}
+                    </tbody>
+                </table >
 
             );
         }
@@ -83,24 +84,23 @@ class ShowUsers extends React.Component {
     async showUserProfile(data) {
         const stat = await statRetrievebyId(data._id);
 
-        this.setState({ popUpBar: "profile", targetStatistic: stat, target: data })
-        console.log("s", this.state.targetStatistic, this.state.popUpBar)
+        this.setState({ popUpBar: "profile", targetStatistic: stat, target: data });
+        console.log("s", this.state.targetStatistic, this.state.popUpBar);
     }
 
-    /*popUp() {
-        if (this.state.popUpBar === "profile") {
-            return (
-                <div>
-                    OuO
-                    {/*<div id="shadowLayer"></div>
-                    <button className="closeButton" onClick={() => { this.setState({ popUpBar: "" }); this.props.setOverflow(1); }}>x</button>
-                    <div className="popUp">
-                        <Profile stat={this.state.targetStatistic} displayName={this.state.target.displayName} username={this.state.target.username} friend={true} />
-            </div>
-                </div>
-            )
-        }
-    }*/
+    resetUserPassword(e, data) {
+        e.stopPropagation()
+        this.setState({ popUpBar: "resetPassword", target: data });
+    }
+
+    setOverflow(val) {
+        this.setState({ overflow: val });
+    }
+
+    handleInnerPopClose() {
+        this.setState({ popUpBar: "" });
+    }
+
 
     popUp() {
         if (this.state.popUpBar === "profile") {
@@ -110,6 +110,17 @@ class ShowUsers extends React.Component {
                     <button className="closeButton" onClick={() => { this.setState({ popUpBar: "" }); this.props.setOverflow(1); }}>x</button>
                     <div className="popUp">
                         <Profile stat={this.state.targetStatistic} displayName={this.state.target.displayName} username={this.state.target.username} friend={true} />
+                    </div>
+                </div>
+            );
+        }
+        else if (this.state.popUpBar === "resetPassword") {
+            return (
+                <div>
+                    <div id="shadowLayer"></div>
+                    <button className="closeButton" onClick={() => { this.setState({ popUpBar: "" }); this.props.setOverflow(1); }}>x</button>
+                    <div className="popUp">
+                        <ChangePassword id={this.state.target._id} handlePopClose={this.handleInnerPopClose} setOverflow={this.props.setOverflow} loginPage={false} />
                     </div>
                 </div>
             );
