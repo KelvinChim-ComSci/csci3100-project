375d273012137c6c7e82d72fc64c8f01c015677e
gutgut2121 <2121haisakisami@gmail.com>
2022-05-04 23:13:14 +0800

    add friend action comment

--- src/frontend/friendlistFunctions/friendActions.js
+++ src/frontend/friendlistFunctions/friendActions.js
@@ -1,4 +1,12 @@
+/**************************************************************************************** 
+This component is activated after clicking the "Gift!" button in the friend list. By
+clicking the button, the system will send the gift to friend and update to the database.
+After the friend has received the gift, their stamina will increase.
 
+Last updated: 29/4/2022 by Chim Ka Chun
+****************************************************************************************/
+
+//Get the current time
 export function getFormattedTime() {
     const currentFullDate = new Date();
     let currentDate = `${currentFullDate.getFullYear()}-${('0' + (currentFullDate.getMonth() + 1)).slice(-2)}-${('0' + currentFullDate.getDate()).slice(-2)}`;
@@ -7,6 +15,7 @@ export function getFormattedTime() {
     return [currentDate, currentTime];
 }
 
+// Save the gift to the database
 export async function giftToBackend(userId, friendId) {
     return await fetch(process.env.REACT_APP_BASE_URL + "/friend/sendGiftToFriend", {
         method: "POST",
